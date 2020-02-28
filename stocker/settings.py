95a763ca8c22088542a7205d058a11a986f57bb8
Ondra Lengal <ondra.lengal@gmail.com>
2020-02-28 20:04:13 +0100

    :sparkles: creating some model

--- stocker/settings.py
+++ stocker/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'stockerapp.apps.StockerappConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- stockerapp/models.py
+++ stockerapp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+# A class representing a stock
+class Stock(models.Model):
+    ticker = models.CharField(max_length=10)
+
+class Order(models.Model):
+    stock = models.ForeignKey(Stock, on_delete=models.CASCADE)
+    amount = models.IntegerField(default=0)
+    unit_price = models.DecimalField(max_digits=20, decimal_places=6)
+    fee = models.DecimalField(max_digits=20, decimal_places=6)
+    date = models.DateTimeField('Execution date and time')
+
